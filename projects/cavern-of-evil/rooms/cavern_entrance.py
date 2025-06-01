429ab3a18a6eaac2e7833a286b5fcaaee5e30563
invictustitan2 <obeyvanta@aperion.cc>
2025-06-01 02:04:28 -0500

    coala1

--- projects/cavern-of-evil/rooms/cavern_entrance.py
+++ projects/cavern-of-evil/rooms/cavern_entrance.py
@@ -1,4 +1,6 @@
 from utils.display import slow_print
 
+
 def enter():
-    slow_print("\n[ROOM] You stand at the entrance of the Cavern. Neon flickers in the distance.", 0.03)
+    slow_print(
+        "\n[ROOM] You stand at the entrance of the Cavern. Neon flickers in the distance.", 0.03)
